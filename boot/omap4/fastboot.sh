66f08aaa183c741eb174210116e7dee1511ab391
Leed Aguilar <leed.aguilar@ti.com>
2011-03-09 23:32:34 -0600

    fastboot: prepare fastboot for future Android flavors
    Now the Android flavor has to be passed as an
    argument to differentiate flash procedures
    
    usage:
    
    ./fastboot.sh [flavor]
    
    @flavor: gingerbread, froyo, eclair, donut
    
    Change-Id: I202775b73d8184b3885773a8628cc9119f14866f
    Signed-off-by: Leed Aguilar <leed.aguilar@ti.com>

--- boot/omap4/fastboot.sh
+++ boot/omap4/fastboot.sh
@@ -1,28 +1,75 @@
 #!/bin/bash
-if [ "$1" != "froyo" ] ; then
-  #Format the gpt on storage device
-  echo "Flashing Android Gingerbread release"
-  #TI specific boot loader flashing
-  ./fastboot flash xloader ./MLO
-  ./fastboot flash bootloader ./u-boot.bin
-  ./fastboot reboot-bootloader
-sleep 5
-  ./fastboot oem format
 
-else
-  echo "Flashing Android Froyo release"
-  ./fastboot flash ptable ./mbr.bin
-  ./fastboot flash environment ./env.txt
-  ./fastboot flash xloader ./MLO
-  ./fastboot flash bootloader ./u-boot.bin
-fi
 
-#Generic Android partitions
-./fastboot flash boot ./boot.img
-./fastboot flash system ./system.img
-if [ "$1" != "froyo" ] ; then
-  ./fastboot flash userdata ./userdata.img
-else
-  ./fastboot flash userdata ./data.img
-fi
-./fastboot flash cache ./cache.img
+# =============================================================================
+# Local variables
+# =============================================================================
+
+flavor="$1"
+fastboot="./fastboot"
+
+# =============================================================================
+# Functions
+# =============================================================================
+
+usage() {
+	cat <<-EOF >&2
+	----------------------------------------------
+	 Usage:
+
+	 fastboot.sh [flavor]
+
+	 @ flavor: correspond to the Android release:
+	           gingerbread, froyo, eclair, donut
+
+	----------------------------------------------
+	EOF
+	exit 1
+}
+
+
+
+# =============================================================================
+# Main
+# =============================================================================
+
+
+
+case $flavor in
+"gingerbread")
+	#Format the gpt on storage device
+	echo "Flashing Android $flavor release"
+	#TI specific boot loader flashing
+	$fastboot flash xloader ./MLO
+	$fastboot flash bootloader ./u-boot.bin
+	$fastboot reboot-bootloader
+	sleep 5
+	$fastboot oem format
+	#Generic Android partitions
+	$fastboot flash boot ./boot.img
+	$fastboot flash system ./system.img
+	# Gingerbread specific
+	$fastboot flash userdata ./userdata.img
+	# Generic Android partitions
+	$fastboot flash cache ./cache.img
+	;;
+"froyo" | "eclair" | "donut")
+	echo "Flashing Android $flavor release"
+	$fastboot flash ptable ./mbr.bin
+	$fastboot flash environment ./env.txt
+	$fastboot flash xloader ./MLO
+	$fastboot flash bootloader ./u-boot.bin
+	# Generic Android partitions
+	$fastboot flash boot ./boot.img
+	$fastboot flash system ./system.img
+	# Froyo specific
+	$fastboot flash userdata ./data.img
+	# Generic Android partitions
+	$fastboot flash cache ./cache.img
+	;;
+*)
+	usage
+	;;
+esac
+
+exit 0
