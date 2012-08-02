fa40fc1ebff8613f2cb4481b25bd8684b64d73d4
James W. Mills <jameswmills@ti.com>
2012-08-02 15:23:26 -0500

    Modified parser to account for new google-added seperators
    Google has added a new field in the manifest that uses colons to
    seperate data.  This script, as it is not using a real XML parser, was
    also arbitrarily using colons as seperators.  For the short term, I've
    modified the internal seperator to be a caret.
    
    I've also removed a lot of whitespace, and added "--no-pager" to the
    git command to avoid being piped through less.
    
    Change-Id: I3f8c0abe58c09585dcdc9fa03dff0d6a3fa5366a
    Signed-off-by: James W. Mills <jameswmills@ti.com>

--- scripts/difftool.sh
+++ scripts/difftool.sh
@@ -20,7 +20,7 @@
 #  0.9 2010-09-08 Jaime A. Garcia <jagarcia@ti.com> Created initial version
 #
 ######
-# 
+#
 # Usage -
 #
 # this tool has the following operation modes:
@@ -31,7 +31,7 @@
 #
 # dependencies: git, diffutils, wget
 # Description:	This script calculate the difference between two xml manifest
-# 				files, then parses its results to git and print out the 
+# 				files, then parses its results to git and print out the
 # 				git log of all repositories that changed.
 ### END INFO
 
@@ -62,7 +62,8 @@ max_tries=5 	# How many tries to fetch previous manifest file
 # used tools and their opts
 diff=/usr/bin/diff
 diff_opts="-E -b -w -B"
-git_opts=${git_opts-"--pretty=format:'%h#%s#%cd#%cn#'"}
+git_opts=${git_opts-"--no-pager"}
+git_log_opts=${git_log_opts-"--pretty=format:'%h#%s#%cd#%cn#'"}
 wget_opts="--tries=2 -nd"
 
 # tool options
@@ -74,7 +75,7 @@ usage () {
 
 log () {
 	if [ "${OUT_DEVICE}" = "file" ]; then
-		echo "`date +%b\ %d\ %T` `basename $0`: $1" 1>>$LOGFILE	
+		echo "`date +%b\ %d\ %T` `basename $0`: $1" 1>>$LOGFILE
 	fi
 	echo "`date +%b\ %d\ %T` `basename $0`: $1"
 }
@@ -116,7 +117,7 @@ get_current_manifest () {
     cur_download_location="http://omapssp.dal.design.ti.com/$(echo ${CLEARCASE_UPLOAD_DIR}|sed -e 's#/vobs/wtbu/#VOBS/#')/L${RELANDDATE}"
     cur_manifest="L${RELANDDATE}_manifest.xml"
 
-    # try to locate current manifest file 
+    # try to locate current manifest file
 	#
 	#we changed the method, now we will try to fetch the cur manifest file locally
     #wget ${wget_opts} -a ${LOGFILE} -O ${MFST_STORE_DIR}/${cur_manifest} ${cur_download_location}/configuration/${cur_manifest}
@@ -190,12 +191,12 @@ search_previous_manifest () {
 get_kernel_diffs () {
 	# once we obtained repo diffs then we fetch kernel patch diffs
 	if [ "${OUT_DEVICE}" = "stdout" ]; then
-		(cd ${YOUR_PATH}/${KERNEL_DIR}; git log ${git_opts} ${PREV_KERNEL_ID}..${CUR_KERNEL_ID}) 2>&1 
+		(cd ${YOUR_PATH}/${KERNEL_DIR}; git ${git_opts} log ${git_log_opts} ${PREV_KERNEL_ID}..${CUR_KERNEL_ID}) 2>&1
 	else
     		echo "********** KERNEL DIFFS **************" >>$OUTFILE
     		echo "cur_kernel_commit_id is ${CUR_KERNEL_ID}" >>$OUTFILE
 	    	echo "prev_kernel_commit_id is ${PREV_KERNEL_ID}" >>$OUTFILE
-		(cd ${YOUR_PATH}/${KERNEL_DIR}; git log ${git_opts} ${PREV_KERNEL_ID}..${CUR_KERNEL_ID}) 2>&1 >>$OUTFILE
+		(cd ${YOUR_PATH}/${KERNEL_DIR}; git ${git_opts} log ${git_log_opts} ${PREV_KERNEL_ID}..${CUR_KERNEL_ID}) 2>&1 >>$OUTFILE
 	fi
 
 	if [ $? -ne 0 ];then
@@ -206,11 +207,11 @@ get_kernel_diffs () {
 }
 
 get_repo_diff () {
-# Extract only the differential tags 
-# eliminate white spaces at the begining 
+# Extract only the differential tags
+# eliminate white spaces at the begining
 # and substitute the middle whitespaces (separators) with ":"
 
-diff_tags=`$diff $diff_opts $OLD_MFST $NEW_MFST |sed -n '/<project.*>/p' |sed 's/[\r\n]/\n/g' |sed 's/^<\s*</<change=\"out\" /g' |sed 's/^>\s*</<change=\"in\" /g' |sed 's/^\s*//g' |sed 's/\s/:/g'`
+diff_tags=`$diff $diff_opts $OLD_MFST $NEW_MFST |sed -n '/<project.*>/p' |sed 's/[\r\n]/\n/g' |sed 's/^<\s*</<change=\"out\" /g' |sed 's/^>\s*</<change=\"in\" /g' |sed 's/^\s*//g' |sed 's/\s/^/g'`
 
 if [ -e ${OUTFILE} ] && [ "${OUT_DEVICE}" = "file" ]; then
 	cat /dev/null > ${OUTFILE}
@@ -218,16 +219,15 @@ fi
 
 for tag in $diff_tags
 do
-	fields=`echo $tag |sed 's/<//' |sed 's/\/>//g' |sed 's/\"//g' | awk -F: '{ print $1, $2, $3, $4, $5, $6}'`
+	fields=`echo $tag |sed 's/<//' |sed 's/\/>//g' |sed 's/\"//g' | awk -F^ '{ print $1, $2, $3, $4, $5, $6}'`
 	name=undefined
 	path=undefined
 	revision=undefined
 
 	for data in $fields
 	do
-		attr=`echo $data |awk -F\= '{ print $1 }'` 
+	        attr=`echo $data |awk -F\= '{ print $1 }'`
 		val=`echo $data |awk -F\= '{ print $2 }'`
-
 		case $attr in
 			name)
 				name=$val
@@ -246,7 +246,7 @@ do
 	if [ "$change" = "out" ]; then
 		continue
 	fi
-	
+
 	name_temp=`echo $name |sed 's/\//>/g'`
 
 	diff_tmp=`$diff $diff_opts $OLD_MFST $NEW_MFST |sed -n '/<project.*>/p' |sed 's/[\r\n]/\n/g' |sed 's/^<\s*</<change=\"out\" /g' |sed 's/^>\s*</<change=\"in\" /g' |sed 's/^\s*//g' |sed 's/>$//g'|sed 's/\s/:/g' |sed 's/\//>/g' |sed -n "/${name_temp}/p"`
@@ -280,10 +280,12 @@ do
 	# better get git log in a subshell
 	if [ "${OUT_DEVICE}" = "stdout" ]; then
 		echo "Project ${name}" 2>&1
-		(cd $REPODIR/$path; git log ${git_opts} ${revision_initial}..${revision_final}) 2>&1 
+		(cd $REPODIR/$path; git ${git_opts} log ${git_log_opts} ${revision_initial}..${revision_final}) 2>&1
+		#just blank line among tree commits (as separator)
+	        echo ""
 	else
 		echo "Project ${name}" >>$OUTFILE
-		(cd $REPODIR/$path; git log ${git_opts} ${revision_initial}..${revision_final}) >>$OUTFILE 2>>${LOGFILE}
+		(cd $REPODIR/$path; git ${git_opts} log ${git_log_opts} ${revision_initial}..${revision_final}) >>$OUTFILE 2>>${LOGFILE}
 		#just blank line among tree commits (as separator)
 	    echo "" >>$OUTFILE
 	fi
@@ -294,7 +296,7 @@ done
 }
 
 #################
-#		main 
+#		main
 #################
 
 checkdeps
@@ -330,7 +332,7 @@ elif [ "${MODE}" = "kernel_diff" ]; then
 	get_kernel_diffs
 	exit 0
 elif [ "${MODE}" = "automatic" ]; then
-	
+
 	get_current_manifest
 	get_current_kernel_commit_id
 
@@ -353,7 +355,7 @@ elif [ "${MODE}" = "automatic" ]; then
 			prev_download_location=`echo ${PREV_MANIFEST%/*} |sed 's/\/configuration//'`
 
 			log "PREV_MANIFEST found. Manifest file downloaded from ${PREV_MANIFEST}"
-		
+
 			OUTFILE=${PREV_OUT_FILE}
 			NEW_MFST=$MFST_STORE_DIR/${cur_manifest}
 			OLD_MFST=$MFST_STORE_DIR/${previous_manifest}
